63c29ae613ea7533f134b124fd6fb7ccca9ae935
Noah Hardy <hardysnoah@icloud.com>
2024-04-09 22:52:18 -0700

    Main file updated with bevy application boilerplate and bevy_editor_pls plugin.
    Main.rs: https://gist.github.com/nshardy/fa45d6144bf89d69a7f6b606cf50185b

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+use bevy_editor_pls::EditorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(EditorPlugin::default())
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera3dBundle::default());
 }
